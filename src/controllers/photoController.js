51c188c6b8b4449fc890f46018ba4d5136bb4fea
Mario1515 <126467500+Mario1515@users.noreply.github.com>
2023-10-02 18:55:16 +0300

    Photo Model
    Working on Photo model

---
+++ src/controllers/photoController.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+const photoManager = require("../managers/photoManager");
+const {getErrorMessage} = require("../utils/errorHelpers");
+
+router.get("/create", (req, res) => {
+    res.render("photos/create");
+});
+
+router.post("/create", async (req, res) => {
+    const photoData = req.body;
+    
+    try{
+        await photoManager.create(photoData);
+        res.redirect("/photos");
+    } catch(err) {
+        res.render("photos/create", { error: getErrorMessage(err) });
+    }
+});
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const routes = require("./routes");
 
 const app = express();
 
-const PORT = 5000;
+const PORT = 3000;
 
 mongoose.connect("mongodb://127.0.0.1:27017/petstagram")
 .then(() => console.log("DB Connected Succesfully"))
