1593edaeb60164675e26c0148b0e28c5520f2abf
Mario1515 <126467500+Mario1515@users.noreply.github.com>
2023-10-03 20:01:21 +0300

    Visibility options on Photo
    working on PhotoController / Edit. delete buttons for guest user etc.

--- src/controllers/photoController.js
+++ src/controllers/photoController.js
@@ -6,7 +6,7 @@ const {getErrorMessage} = require("../utils/errorHelpers");
 router.get("/", async (req, res) => {
     const photos = await photoManager.getAll().lean();
 
-    res.render("photos", {  photos  })
+    res.render("photos", {  photos })
 });
 
 router.get("/create", (req, res) => {
@@ -26,5 +26,14 @@ router.post("/create", async (req, res) => {
         res.render("photos/create", { error: getErrorMessage(err) });
     }
 });
+
+router.get("/:photoId/details", async (req, res) => {
+    const photoId = req.params.photoId;
+    const photo = await photoManager.getOne(photoId).lean();
+    const isOwner = req.user?._id == photo.owner._id; // checking if there is a user id
+
+    res.render("photos/details", { photo, isOwner });
+});
+
 
 module.exports = router;
